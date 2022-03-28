165e3f257828adc33c9c0275118f3151940e0cca
Javier Matejko <javiermatejko@pathfindersstudio.com>
2022-03-28 15:12:57 -0300

    Added simple player movement.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform playerBody = null;
+    #endregion
+
+    #region PRIVATE_FIELDS
+    private float xRotation = 0f;
+    #endregion
+
+    #region UNITY_CALLS
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    #endregion
+
+    #region INITIALIZATION
+
+    #endregion
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private Transform groundCheck = null;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    #endregion
+
+    #region PRIVATE_FIELDS
+    private CharacterController cc = null;
+    private Vector3 velocity = new Vector3();
+    private bool isGrounded = false;
+    #endregion
+
+    #region UNITY_CALLS
+    private void Start()
+    {
+        cc = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        cc.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        cc.Move(velocity * Time.deltaTime);
+    }
+    #endregion
+
+    #region INITIALIZATION
+
+    #endregion
+}
