09d87134cf07d78a44a0f352b8dda0c72b253cfa
Javier Matejko <javiermatejko@pathfindersstudio.com>
2022-03-29 08:50:53 -0300

    Added range variable to Gun script.

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -6,12 +6,14 @@ public class Gun : ScriptableObject
     #region EXPOSED_FIELDS
     [SerializeField] private int maxAmmo = 10;
     [SerializeField] private int currentAmmo = 10;
+    [SerializeField] private float range = 100f;
     [SerializeField] private GameObject gunPrefab = null;
     #endregion
 
     #region PROPERTIES
     public int MaxAmmo { get => maxAmmo; }
     public int CurrentAmmo { get => currentAmmo; }
+    public float Range { get => range; }
     public GameObject GunPrefab { get => gunPrefab; }
     #endregion
 }
